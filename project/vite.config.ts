f659ed8a12bb08c29896a06798a0e0fe49aeae1e
ganondorofu <167788682+ganondorofu@users.noreply.github.com>
2024-12-22 23:56:37 +0900

    vite.config.tsのbaseパスを正確に指定

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<stem-com>/', // リポジトリ名を実際のリポジトリ名に置き換えてください
+  base: '/stem-com/', // リポジトリ名を正確に指定
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'], // 必要に応じて保持または削除
   },
-});//a
+});
