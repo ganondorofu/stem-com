ddaf0b81f42642d402b0ea96c25b85a444f5fc58
ganondorofu <ganondorofu3143@outlook.com>
2024-12-22 23:59:07 +0900

    vite.config.tsにPermissions-Policyヘッダーを追加

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/stem-com/', // リポジトリ名を正確に指定
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'], // 必要に応じて保持または削除
+  server: {
+    headers: {
+      'Permissions-Policy': '', // interest-cohortを削除
+    },
   },
+  plugins: [react()],
+  base: '/stem-com/', // 必要に応じて変更
 });
